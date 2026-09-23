2c9808c98ec66a46264a94d83b568fa9640c10db
Alihan Tangül <tangulalihan@gmail.com>
2026-09-24 02:02:20 +0300

    feat(utils): manhattan distance

--- utils/distances.py
+++ utils/distances.py
@@ -13,5 +13,6 @@ def manhattan(point1, point2):
     """
     d_x = abs(point2[0] - point1[0])
     d_y = abs(point2[1] - point1[1])
+    distance = d_x + d_y
 
-    return d_x + d_y
+    return distance
