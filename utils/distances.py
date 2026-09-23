57014a792c123a0b97bdf4f09950b80455e8b2d9
Alihan Tangül <tangulalihan@gmail.com>
2026-09-23 23:20:59 +0300

    Son değişiklikler eklendi

--- utils/distances.py
+++ utils/distances.py
@@ -1,1 +1,17 @@
 pass  # YOUR CODE HERE
+
+def manhattan(point1, point2):
+    """
+    Calculate the Manhattan distance between two points.
+
+    Parameters:
+    point1 (tuple): The first point as a tuple of coordinates (x1, y1).
+    point2 (tuple): The second point as a tuple of coordinates (x2, y2).
+
+    Returns:
+    int: The Manhattan distance between the two points.
+    """
+    d_x = abs(point2[0] - point1[0])
+    d_y = abs(point2[1] - point1[1])
+
+    return d_x + d_y
