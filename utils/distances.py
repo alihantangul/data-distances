850f2ba10168a029a3579e82fad80f41671198f3
Alihan Tangül <tangulalihan@gmail.com>
2026-09-24 02:21:26 +0300

    euclidian test is succesful with pytest

--- utils/distances.py
+++ utils/distances.py
@@ -18,8 +18,8 @@ def manhattan(point1, point2):
     return distance
 
 def euclidean(a, b):
-    d_x = b[0] - a[0]
-    d_y = b[1] - a[1]
+        d_x = b[0] - a[0]
+        d_y = b[1] - a[1]
 
-    distance = d_x ** 2 + d_y ** 2
-    return distance
+        distance = (d_x ** 2 + d_y ** 2) ** 0.5
+        return distance
